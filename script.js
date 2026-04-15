f7877466dfcc0cb1ce3806a15166fc75ba9688dd
Jānis Lovcevičs <A250338JL@rvt.lv>
2026-04-15 12:52:27 +0300

    Started js

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch('./products_data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();  
+    })
+    .then(data => console.log(data))  
+    .catch(error => console.error('Failed to fetch data:', error));
